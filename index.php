f8e7cf8737d95d1a6cd87083cf271c6ea03caa54
Reinaldo de Carvalho <reinaldo.decarvalho@dataprev.gov.br>
2015-04-17 16:12:26 -0300

    Import inicial.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="css/styles.css" type="text/css">
+  <title>Agenda PHP</title>
+</head>
+<body>
+
+<div id="header">
+  <h1><a href="#">Agenda PHP</a></h1>
+  <ul id="menu">
+    <li><a href="#">listar</a></li>
+    <li><a href="#">adicionar</a></li>
+    <li><a href="#">editar</a></li>
+    <li><a href="#">excluir</a></li>
+    <li><a href="#">pesquisar</a></li>
+  </ul>
+</div>
+
+<?php
+
+?>
+
+</body>
+</html>
---
+++ tipos.php
@@ -0,0 +1,27 @@
+<?php
+
+class Operadora {
+	function Operadora() {
+		$this->nome;
+	}
+}
+
+class Telefone {
+	function Telefone() {
+		$this->ddd;
+		$this->numero;
+		$this->operadora;
+	}
+}
+
+class Contato {
+	function Contato() {
+		$this->id;
+		$this->nome;
+		$this->email;
+		$this->notas;
+		$this->telefones;
+	}
+}
+
+?>
